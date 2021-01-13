afc22b7b0a6495131953ed8ae11595a4848c77f4
RafaeLS85 <rafaelsebastian@live.com.ar>
2021-01-13 20:14:12 -0300

    docs

---
+++ docs/precache-manifest.9e3d7af6a26bc0cf05cd41e3ba5a2b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0877afc9b297200c253029ab4305869a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c394bfcf28b45d515bd7",
+    "url": "/static/css/main.6e580c0a.chunk.css"
+  },
+  {
+    "revision": "340f84242d4b75926707",
+    "url": "/static/js/2.b32a697d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b32a697d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c394bfcf28b45d515bd7",
+    "url": "/static/js/main.e5a60537.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
